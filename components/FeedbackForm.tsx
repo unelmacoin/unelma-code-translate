f592f860e9eaa2701f8aebd4ff54485da363088c
Dibya <dahal.dibya7@gmail.com>
2023-07-25 23:25:55 +0300

    UP-50 label changed

--- components/FeedbackForm.tsx
+++ components/FeedbackForm.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { modalControl } from '@/types/types';
 
-
-const FeedbackForm: React.FC<modalControl> = ({modal, setModal}) => {
-emailjs.init("Y8Uhbou9I9l2yDoUh");
+const FeedbackForm: React.FC<modalControl> = ({ modal, setModal }) => {
+  emailjs.init('Y8Uhbou9I9l2yDoUh');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
 
@@ -12,29 +11,31 @@ emailjs.init("Y8Uhbou9I9l2yDoUh");
     setName(event.target.value);
   };
 
-  const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleMessageChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
     setMessage(event.target.value);
   };
 
-  const templateParams ={
-    user:name,
-    message:message,
-    reply_to:"dahal.dibya7@gmail.com"
-
-  }
+  const templateParams = {
+    user: name,
+    message: message,
+    reply_to: 'dahal.dibya7@gmail.com',
+  };
 
   const handleSubmit = () => {
     if (name.trim() === '' || message.trim() === '') {
       alert('Please fill out both the name and message fields.');
     } else {
-        emailjs.send('service_poz1s4s','template_ogcro9v', templateParams)
-        .then((response) => {
-           console.log('SUCCESS!', response.status, response.text);
-           setModal(false);
-        }, (err) => {
-           console.log('FAILED...', err);
-           
-        });
+      emailjs.send('service_poz1s4s', 'template_ogcro9v', templateParams).then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          setModal(false);
+        },
+        (err) => {
+          console.log('FAILED...', err);
+        },
+      );
 
       console.log(`Name: ${name}`);
       console.log(`Message: ${message}`);
@@ -44,56 +45,57 @@ emailjs.init("Y8Uhbou9I9l2yDoUh");
     }
   };
 
- const closeModal= () => {
+  const closeModal = () => {
     setModal(false);
   };
 
   return (
-    <div className=" z-20 feedback-form min-h-screen min-w-full flex items-center justify-center bg-[#0E1117] bg-opacity-50 absolute p-4">
-    <div className="fixed top-0 right-0 transform h-screen w-auto flex flex-col gap-6 p-8 rounded-lg bg-white text-neutral-200 shadow-lg">
-    <button
-  onClick={closeModal}
-  className="self-end text-black text-xl font-bold leading-none focus:outline-none shadow-lg hover:bg-gray-200 hover:text-gray-800 active:bg-gray-300 active:shadow-inner"
->
-  X
-</button>
+    <div className=" feedback-form absolute z-20 flex min-h-screen min-w-full items-center justify-center bg-[#0E1117] bg-opacity-50 p-4">
+      <div className="fixed right-0 top-0 flex h-screen w-auto transform flex-col gap-6 rounded-lg bg-white p-8 text-neutral-200 shadow-lg">
+        <button
+          onClick={closeModal}
+          className="self-end text-xl font-bold leading-none text-black shadow-lg hover:bg-gray-200 hover:text-gray-800 focus:outline-none active:bg-gray-300 active:shadow-inner"
+        >
+          X
+        </button>
 
-      <h1 className="text-2xl text-[#0E1117] font-bold">Send feedback to Unelma Platforms</h1>
-      <div>
-        <label htmlFor="name" className="block text-[#0E1117] text-lg">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={handleNameChange}
-          placeholder="Enter your name"
-          className="w-full px-4 py-2 mt-2 border text-black border-gray-300 rounded-md"
-        />
+        <h1 className="text-2xl font-bold text-[#0E1117]">
+          Send feedback to Unelma Platforms
+        </h1>
+        <div>
+          <label htmlFor="name" className="block text-lg text-[#0E1117]">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+            className="mt-2 w-full rounded-md border border-gray-300 px-4 py-2 text-black"
+          />
+        </div>
+        <div>
+          <label htmlFor="message" className="block text-lg text-[#0E1117]">
+          Tell us what prompted this feedback
+          </label>
+          <textarea
+            id="message"
+            rows={12}
+            value={message}
+            onChange={handleMessageChange}
+            placeholder="Enter your message here"
+            className="mt-2 w-full rounded-md border border-gray-300 px-4 py-2 text-black "
+          />
+        </div>
+        <button
+          onClick={handleSubmit}
+          className="mt-4 w-full rounded-md bg-[#0E1117] px-4 py-2 text-lg font-semibold text-white hover:bg-gray-600 focus:outline-none"
+        >
+          Submit
+        </button>
       </div>
-      <div>
-        <label htmlFor="message" className="block text-[#0E1117] text-lg">
-          Message
-        </label>
-        <textarea
-          id="message"
-          rows={12}
-          value={message}
-          onChange={handleMessageChange}
-          placeholder="Enter your message here"
-          className="w-full px-4 py-2 mt-2 border text-black border-gray-300 rounded-md "
-        />
-      </div>
-      <button
-        onClick={handleSubmit}
-        className="w-full px-4 py-2 mt-4 text-lg font-semibold text-white bg-[#0E1117] rounded-md hover:bg-gray-600 focus:outline-none"
-      >
-        Submit
-      </button>
     </div>
-    </div>
-
   );
 };
 
