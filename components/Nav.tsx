30fe5fe69004705a8b77a93372770641b83d2b68
Dibya <dahal.dibya7@gmail.com>
2023-09-25 17:12:19 +0300

    UP-105 bg fixed and other css changes

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-const Nav: React.FC = () => {
+interface themeBtn {
+  isDark:boolean;
+}
+
+const Nav: React.FC<themeBtn> = ({isDark}
+) => {
   return (
     <nav>
-      <div className=" flex w-100 h-10 relative flex-wrap items-center justify-between py-2 cursor-pointer">
+      <div className=" sm:px-10 flex w-100 relative flex-wrap items-center justify-between py-2 px-4 cursor-pointer">
         <a  href="https://translate.u16p.com/">
-        <h1 className='text-2xl font-medium pl-10 '>Unelma-Code Translator</h1>
+        <h1 className='text-2xl font-medium '>Unelma-Code Translator</h1>
         </a>
-        <a className='bg-transparent pr-10' href="https://u16p.com/">
+        <a className={
+           isDark
+          ? 'text-[#FFFFFF80] '
+          : 'text-[#00000080] '} href="https://u16p.com/">
           Back to U16P
         </a>
         </div>
