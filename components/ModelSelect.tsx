1df59486672a1322f5471b74d4efaf68c5dc61bc
Dibya <dahal.dibya7@gmail.com>
2023-10-04 15:58:49 +0300

    UP-53 enabled button for GPT-4

--- components/ModelSelect.tsx
+++ components/ModelSelect.tsx
@@ -20,8 +20,8 @@ export const ModelSelect: FC<Props> = ({ model, onChange, isDark }) => {
       value={model}
       onChange={handleChange}
     >
-      <option value="gpt-3.5-turbo">GPT-3.5</option>
-      {/* <option value="gpt-4">GPT-4</option> */}
+      <option >GPT-3.5</option>
+      <option value="gpt-4">GPT-4</option>
     </select>
   );
 };
