034f1b32769f991f808adee911ed718293df424f
SantoshChapagai <santoshchapagai100@gmail.com>
2024-04-11 10:32:34 +0300

    add mic icon in the input field and add functionality to handle the voice input

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from "react";
+import { useSpeech } from "@/hooks/useSpeech";
+import { useEffect, useRef, useState } from "react";
+import { IoMdMic, IoMdMicOff } from "react-icons/io";
 
 interface Props {
   text: string;
   editable?: boolean;
   onChange?: (value: string) => void;
-  isDark:boolean;
-  maxCharacterCount:number;
+  isDark: boolean;
+  maxCharacterCount: number;
 }
 
 export const TextBlock: React.FC<Props> = ({
@@ -15,32 +17,70 @@ export const TextBlock: React.FC<Props> = ({
   isDark,
   maxCharacterCount = 5000,
 }) => {
-  const bg = isDark ? 'bg-[#1A1B26]': 'bg-[#fff]';
-  const textColor = isDark ? 'text-neutral-200 ': 'text-black';
+  const bg = isDark ? 'bg-[#1A1B26]' : 'bg-[#fff]';
+  const textColor = isDark ? 'text-neutral-200 ' : 'text-black';
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [speechText, setSpeechText] = useState<string>('');
+
   useEffect(() => {
     if (editable && textareaRef.current) {
       textareaRef.current.focus();
     }
   }, [editable]);
+
+  const {
+    textSpeech,
+    isListening,
+    handleListening,
+    hasRecognitionSupport,
+  } = useSpeech();
+
+  useEffect(() => {
+    if (textSpeech) {
+      setSpeechText(textSpeech);
+    }
+  }, [textSpeech]);
+
+  useEffect(() => {
+    if (text !== speechText) {
+      onChange(speechText);
+    }
+  }, [speechText, text, onChange]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+    setSpeechText(value);
+    onChange(value);
+  };
+
   return (
     <div className="relative">
+      {hasRecognitionSupport ? (
+        <button onClick={handleListening}>
+          {isListening ? (
+            <IoMdMic className="absolute bottom-3 right-20" size={32} title="Click to stop voice input" />
+          ) : (
+            <IoMdMicOff className="absolute bottom-3 right-20" size={32} title="Click to start voice input" />
+          )}
+        </button>
+      ):(
+        <h1>Your browser does not support voice recognition</h1>
+      )}
       <textarea
-      ref={textareaRef}
+        ref={textareaRef}
         className={`min-h-[500px] ${bg} w-full p-4 text-[15px] focus:outline-none ${textColor} transition-all duration-300`}
         style={{ resize: 'none' }}
-        value={text}
-        onChange={(e) => onChange(e.target.value)}
+        value={text || speechText}
+        onChange={handleInputChange}
         disabled={!editable}
         maxLength={maxCharacterCount}
-        autoFocus = {true}
+        autoFocus={true}
       />
+
       {editable && (
-      <div
-        className="flex justify-end absolute bottom-2 right-3"
-      >
-        {`${text.length}/${maxCharacterCount}`}
-      </div>
+        <div className="flex justify-end absolute bottom-2 right-3">
+          {`${(text.length || speechText.length)}/${maxCharacterCount}`}
+        </div>
       )}
     </div>
   );
