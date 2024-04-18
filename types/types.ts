63bc8698078975d336afb2ef64c8e2b9ca906d8d
SantoshChapagai <santoshchapagai100@gmail.com>
2024-04-18 11:57:43 +0300

    add the gpt 4 turbo model in the list to give wide range of option for the user to select

--- types/types.ts
+++ types/types.ts
@@ -1,4 +1,4 @@
-export type OpenAIModel = 'gpt-3.5-turbo' | 'gpt-4' | 'gpt-4-0125-preview';
+export type OpenAIModel = 'gpt-3.5-turbo' | 'gpt-4' | 'gpt-4-turbo' | 'gpt-4-0125-preview';
 
 export interface TranslateBody {
   inputLanguage: string;
