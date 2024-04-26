d275a27f18fad3412c41705679b283e318fc95c1
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-04-26 11:01:53 +0300

    prettier auto formate double to single quote.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ import HistoryButton from '@/components/HistoryButton';
 import Tesseract from 'tesseract.js';
 import UploadImagesAndFiles from '@/components/UploadImagesAndFiles';
 import { languages } from '@/components/LanguageSelect';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Feedback } from '@/components/Feedback';
@@ -33,7 +33,7 @@ export default function Home() {
   const [historyExpand, setHistoryExpand] = useState<boolean>(false);
 
   useEffect(() => {
-    toast.info("Enter or upload some code in Input");
+    toast.info('Enter or upload some code in Input');
   }, []);
 
   useEffect(() => {
@@ -43,16 +43,15 @@ export default function Home() {
     }
   }, []);
 
- const handleHistoryExpand = ()=>{
-  setHistoryExpand(!historyExpand)
- }
+  const handleHistoryExpand = () => {
+    setHistoryExpand(!historyExpand);
+  };
 
   const handleTranslate = async () => {
-
-     if (inputLanguage === outputLanguage) {
-       alert('Please select different languages.');
+    if (inputLanguage === outputLanguage) {
+      alert('Please select different languages.');
       return;
-     }
+    }
 
     setLoading(true);
     setOutputCode('');
@@ -110,9 +109,12 @@ export default function Home() {
     copyToClipboard(code);
 
     const updatedHistory = new Set([...history, inputCode]);
-  const mergedHistory = new Set([...updatedHistory, ...JSON.parse(localStorage.getItem("userHistory") || "[]")]);
-  setHistory(mergedHistory);
-  localStorage.setItem("userHistory", JSON.stringify([...mergedHistory]));
+    const mergedHistory = new Set([
+      ...updatedHistory,
+      ...JSON.parse(localStorage.getItem('userHistory') || '[]'),
+    ]);
+    setHistory(mergedHistory);
+    localStorage.setItem('userHistory', JSON.stringify([...mergedHistory]));
   };
 
   const copyToClipboard = (text: string) => {
@@ -123,13 +125,12 @@ export default function Home() {
     document.execCommand('copy');
     document.body.removeChild(el);
   };
- 
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       handleTranslate();
     }, 5000);
- 
+
     return () => clearTimeout(delayDebounceFn);
   }, [outputLanguage, inputCode, model]);
 
@@ -140,65 +141,68 @@ export default function Home() {
     }
   }, []);
 
-  useEffect(()=>{
-    if(inputCode.trim()!== "" && loading){
-      toast.info("Translating...")
+  useEffect(() => {
+    if (inputCode.trim() !== '' && loading) {
+      toast.info('Translating...');
     }
-  }, [loading])
+  }, [loading]);
 
   const handleUpload = (file: File) => {
     if (
-      file.type === "application/zip" ||
-      file.type === "application/x-rar-compressed" ||
-      file.type === "application/x-tar" ||
-      file.name.endsWith(".zip") ||
-      file.name.endsWith(".rar") ||
-      file.name.endsWith(".tar")
+      file.type === 'application/zip' ||
+      file.type === 'application/x-rar-compressed' ||
+      file.type === 'application/x-tar' ||
+      file.name.endsWith('.zip') ||
+      file.name.endsWith('.rar') ||
+      file.name.endsWith('.tar')
     ) {
       Swal.fire({
-        icon: "error",
-        text: "You cannot upload zip folders at the moment!",
+        icon: 'error',
+        text: 'You cannot upload zip folders at the moment!',
       });
       return;
     }
     const reader = new FileReader();
-    setHasTranslated(!hasTranslated)
+    setHasTranslated(!hasTranslated);
     reader.onload = async (event) => {
-        if (file.type.startsWith('image/')) {
-            const imageData = event.target?.result as ArrayBuffer;
-            const blob = new Blob([imageData], { type: 'image/*' });
-            const imageUrl = URL.createObjectURL(blob);
+      if (file.type.startsWith('image/')) {
+        const imageData = event.target?.result as ArrayBuffer;
+        const blob = new Blob([imageData], { type: 'image/*' });
+        const imageUrl = URL.createObjectURL(blob);
 
-            const { data: { text } } = await Tesseract.recognize(imageUrl, 'eng');
-            setInputCode(text);
+        const {
+          data: { text },
+        } = await Tesseract.recognize(imageUrl, 'eng');
+        setInputCode(text);
+        setInputLanguage('Natural Language');
+        URL.revokeObjectURL(imageUrl);
+      } else {
+        setInputCode(event.target?.result as string);
+        const fileExtension = file.name.split('.').pop()?.toLowerCase();
+        if (fileExtension) {
+          const detectedLanguage = languages.find(
+            (lang) => lang.value.toLowerCase() === fileExtension,
+          );
+          if (detectedLanguage) {
+            setInputLanguage(detectedLanguage.value);
+            setOutputLanguage('Natural Language');
+          } else {
             setInputLanguage('Natural Language');
-            URL.revokeObjectURL(imageUrl);
-        } else {
-             setInputCode(event.target?.result as string);
-            const fileExtension = file.name.split('.').pop()?.toLowerCase();
-            if (fileExtension) {
-                const detectedLanguage = languages.find(lang => lang.value.toLowerCase() === fileExtension);
-                if (detectedLanguage) {
-                    setInputLanguage(detectedLanguage.value);
-                    setOutputLanguage("Natural Language")
-                } else {
-                    setInputLanguage("Natural Language");
-                }
-            }
-           
+          }
         }
+      }
     };
 
     if (file.type.startsWith('image/')) {
-        reader.readAsArrayBuffer(file);
+      reader.readAsArrayBuffer(file);
     } else {
-        reader.readAsText(file);
+      reader.readAsText(file);
     }
-};
+  };
 
   const handleSwap = () => {
     setInputLanguage(outputLanguage);
-     setOutputLanguage(inputLanguage);
+    setOutputLanguage(inputLanguage);
     setInputCode(outputCode);
     setOutputCode(inputCode);
   };
@@ -208,17 +212,14 @@ export default function Home() {
     localStorage.setItem('unelTheme', JSON.stringify(newIsDark));
   };
 
-  const handleHistorySelect = (value:string) =>{
-  setInputCode(value)
-  }
+  const handleHistorySelect = (value: string) => {
+    setInputCode(value);
+  };
 
-  const bodyBg =
-    isDark === true
-      ? '#0E2810'
-      : '#E0E0E0';
-  const navBg = isDark === true ?'#333333':'#E8EBF5';
+  const bodyBg = isDark === true ? '#0E2810' : '#E0E0E0';
+  const navBg = isDark === true ? '#333333' : '#E8EBF5';
 
-  const changeBodyBackgroundColor = (color:any) => {
+  const changeBodyBackgroundColor = (color: any) => {
     document.body.style.backgroundColor = color;
   };
 
@@ -227,45 +228,46 @@ export default function Home() {
     changeBodyBackgroundColor(backgroundColor);
   }, [isDark]);
 
-  
-
-  useEffect(()=>{
+  useEffect(() => {
     const handleSwap = () => {
       setInputLanguage(outputLanguage);
-       setOutputLanguage(inputLanguage);
+      setOutputLanguage(inputLanguage);
       setInputCode(outputCode);
       setOutputCode(inputCode);
     };
-    const handleKeyboardShortcut = (event: any) =>{
-      if ((event.metaKey || event.ctrlKey) && event.shiftKey && event.key.toUpperCase() === 'S') {
-        handleSwap()
+    const handleKeyboardShortcut = (event: any) => {
+      if (
+        (event.metaKey || event.ctrlKey) &&
+        event.shiftKey &&
+        event.key.toUpperCase() === 'S'
+      ) {
+        handleSwap();
       }
-    }
+    };
     document.addEventListener('keydown', handleKeyboardShortcut);
     return () => {
       document.removeEventListener('keydown', handleKeyboardShortcut);
     };
-  }, [inputCode, inputLanguage, outputCode, outputLanguage])
+  }, [inputCode, inputLanguage, outputCode, outputLanguage]);
 
-  useEffect(()=>{
-    if(outputCode.trim() !== "" && hasTranslated){
-    toast.success("Your code is translated")
+  useEffect(() => {
+    if (outputCode.trim() !== '' && hasTranslated) {
+      toast.success('Your code is translated');
     }
-  },[hasTranslated])
+  }, [hasTranslated]);
 
   return (
-    <div
-     style={{ background: bodyBg}}>
+    <div style={{ background: bodyBg }}>
       <div
         style={{ background: navBg }}
-        className={`fixed top-0 bg-black z-50 ${
+        className={`fixed top-0 z-50 bg-black ${
           isDark
-            ? ' py-4  w-full text-white transition-all duration-300'
-            : 'py-4  w-full  transition-all duration-300'
+            ? ' w-full  py-4 text-white transition-all duration-300'
+            : 'w-full  py-4  transition-all duration-300'
         }`}
       >
         {' '}
-        <Nav isDark={isDark} toggleDarkMode={toggleDarkMode}/>
+        <Nav isDark={isDark} toggleDarkMode={toggleDarkMode} />
       </div>
 
       <div
@@ -275,7 +277,7 @@ export default function Home() {
             : ' text-black transition-all duration-300'
         }
       >
-        <Head >
+        <Head>
           <title className="pt-2">Unelma-Code Translator</title>
           <meta
             name="description"
@@ -285,34 +287,56 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <div className={`pt-4 md:pt-16 flex h-full min-h-fit flex-col flex-wrap px-4 pb-20 sm:px-10 ${historyExpand?"":"items-center"}`}>
-          <div className={`flex flex-col ${historyExpand?"md:items-start":""}justify-center mt-20 lg:mt-10 md:mt-10`}>
+        <div
+          className={`flex h-full min-h-fit flex-col flex-wrap px-4 pb-20 pt-4 sm:px-10 md:pt-16 ${
+            historyExpand ? '' : 'items-center'
+          }`}
+        >
+          <div
+            className={`flex flex-col ${
+              historyExpand ? 'md:items-start' : ''
+            }justify-center mt-20 md:mt-10 lg:mt-10`}
+          >
             <div className="text-4xl font-bold">Unelma-Code Translator</div>
           </div>
 
-          <div className={`mt-2 flex ${historyExpand?"itmes-start lg:items-center":"items-center"}justify-center  space-x-2`}>
+          <div
+            className={`mt-2 flex ${
+              historyExpand ? 'itmes-start lg:items-center' : 'items-center'
+            }justify-center  space-x-2`}
+          >
             <ModelSelect
               model={model}
               isDark={isDark}
               onChange={(value) => setModel(value)}
             />
           </div>
-          <div className={`mt-2 ${historyExpand?"":"text-center"} text-xs`}>
-            {inputCode.trim()!=="" && loading
+          <div className={`mt-2 ${historyExpand ? '' : 'text-center'} text-xs`}>
+            {inputCode.trim() !== '' && loading
               ? 'Translating...'
-              : outputCode.trim()!=="" && hasTranslated
+              : outputCode.trim() !== '' && hasTranslated
               ? 'Your code has been translated!'
               : 'Enter some code in Input'}
           </div>
-          
-          <div className={`mt-6 flex w-full max-w-[1200px] flex-col lg:flex-row justify-center sm:space-x-4 ${historyExpand? "lg:w-2/3 md:flex-col items-center md:items-start": "md:flex-row"}`}>
-            <div className="max-h-200 w-full flex flex-col space-y-2 sm:w-2/4">
-              <div className='flex space-x-4'>
-            <UploadImagesAndFiles onUpload={handleUpload}/>
-            <HistoryButton onSelect={handleHistorySelect} onExpand={handleHistoryExpand} isDark={isDark}/>
-            </div>
+
+          <div
+            className={`mt-6 flex w-full max-w-[1200px] flex-col justify-center sm:space-x-4 lg:flex-row ${
+              historyExpand
+                ? 'items-center md:flex-col md:items-start lg:w-2/3'
+                : 'md:flex-row'
+            }`}
+          >
+            <div className="max-h-200 flex w-full flex-col space-y-2 sm:w-2/4">
+              <div className="flex space-x-4">
+                <UploadImagesAndFiles onUpload={handleUpload} />
+                <HistoryButton
+                  onSelect={handleHistorySelect}
+                  onExpand={handleHistoryExpand}
+                  isDark={isDark}
+                />
+              </div>
               <div className="text-center text-xl font-bold">Input</div>
-           
+
               <LanguageSelect
                 language={inputLanguage}
                 onChange={(value) => {
@@ -333,7 +357,7 @@ export default function Home() {
                   onChange={(value) => {
                     setInputCode(value);
                     setHasTranslated(false);
-                    `${inputCode.length}/5000`
+                    `${inputCode.length}/5000`;
                   }}
                 />
               ) : (
@@ -349,16 +373,24 @@ export default function Home() {
               )}
             </div>
             <div>
-            <IoMdSwap
-            title='Swap languages (Cmd + Shift + S)'
-              onClick={handleSwap}
-              className={`${historyExpand?"lg:mt-20": " mt-0 md:mt-20 lg:mt-20"} cursor-pointer items-center w-12 text-3xl hover:opacity-80 ${
-                isDark ? 'text-white-700' : 'text-black'
-              }`}
-            />
+              <IoMdSwap
+                title="Swap languages (Cmd + Shift + S)"
+                onClick={handleSwap}
+                className={`${
+                  historyExpand ? 'lg:mt-20' : ' mt-0 md:mt-20 lg:mt-20'
+                } w-12 cursor-pointer items-center text-3xl hover:opacity-80 ${
+                  isDark ? 'text-white-700' : 'text-black'
+                }`}
+              />
             </div>
             <div className="flex h-full w-full flex-col justify-center space-y-2 sm:mt-0 sm:w-2/4">
-              <div className={`text-center ${historyExpand?"lg:mt-10":"mt-0 md:mt-10 lg:mt-10"} text-xl font-bold`}>Output</div>
+              <div
+                className={`text-center ${
+                  historyExpand ? 'lg:mt-10' : 'mt-0 md:mt-10 lg:mt-10'
+                } text-xl font-bold`}
+              >
+                Output
+              </div>
 
               <LanguageSelect
                 language={outputLanguage}
@@ -370,19 +402,23 @@ export default function Home() {
               />
 
               {outputLanguage === 'Natural Language' ? (
-                <TextBlock text={outputCode} isDark={isDark} maxCharacterCount={5000} />
+                <TextBlock
+                  text={outputCode}
+                  isDark={isDark}
+                  maxCharacterCount={5000}
+                />
               ) : (
                 <CodeBlock code={outputCode} isDark={isDark} />
               )}
-              <div className=''>
-              <Feedback />
+              <div className="">
+                <Feedback />
               </div>
             </div>
-          </div> 
-        </div> 
+          </div>
+        </div>
       </div>
       <Footer isDark={isDark} />
-      <ToastContainer autoClose={2000} style={{top:"5rem"}}/>
+      <ToastContainer autoClose={2000} style={{ top: '5rem' }} />
     </div>
   );
 }
