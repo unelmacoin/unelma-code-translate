6de1b8488c68f94284adba7f7a8292d45bb8fdf1
Pushpa Gnyawali <75498254+Pushpa88431@users.noreply.github.com>
2024-09-02 23:01:03 +0300

    removed Unelma from page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,29 +2,27 @@ import React, { MouseEventHandler } from 'react';
 import ThemeButton from './ThemeButton';
 
 interface themeBtn {
-  isDark:boolean;
-  toggleDarkMode: MouseEventHandler<HTMLButtonElement>
+  isDark: boolean;
+  toggleDarkMode: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Nav: React.FC<themeBtn> = ({isDark, toggleDarkMode}
-) => {
+const Nav: React.FC<themeBtn> = ({ isDark, toggleDarkMode }) => {
   return (
     <nav>
-      <div className="sm:px-10 flex w-full flex-wrap  items-center justify-between py-2 px-4 cursor-pointer">
-        <a  href="https://translate.u16p.com/">
-        <h1 className='text-2xl font-medium '>Unelma-Code Translator</h1>
+      <div className="flex w-full cursor-pointer flex-wrap  items-center justify-between px-4 py-2 sm:px-10">
+        <a href="https://translate.u16p.com/">
+          <h1 className="text-2xl font-medium ">Code Translator</h1>
         </a>
-        <div className='flex justify-center items-center space-x-4'>
-        <a className={
-           isDark
-          ? 'text-[#FFFFFF] '
-          : 'text-[#000000] '} href="https://u16p.com/">
-          Back to U16P
-        </a>
-        <ThemeButton isDark={isDark} toggleDarkMode={toggleDarkMode}/>
+        <div className="flex items-center justify-center space-x-4">
+          <a
+            className={isDark ? 'text-[#FFFFFF] ' : 'text-[#000000] '}
+            href="https://u16p.com/"
+          >
+            Back to U16P
+          </a>
+          <ThemeButton isDark={isDark} toggleDarkMode={toggleDarkMode} />
         </div>
-        </div>
-        
+      </div>
     </nav>
   );
 };
