914beabfa50cdbf7d1db45d0d469b92479a2b943
Bishnu Suyel <suyel2007@gmail.com>
2025-02-01 20:18:40 +0200

    Add deepseek as a new model type

--- types/types.ts
+++ types/types.ts
@@ -1,11 +1,12 @@
 export type OpenAIModel = 'gpt-3.5-turbo' | 'gpt-4' | 'gpt-4-turbo' | 'gpt-4o' | 'gpt-4o-mini' | 'gpt-4-0125-preview' | 'o1-preview' | 'o1-mini';
 export type xAI = 'grok-2-latest';
+export type OpenAI = 'deepseek-chat';
 
 export interface TranslateBody {
   inputLanguage: string;
   outputLanguage: string;
   inputCode: string;
-  model: OpenAIModel | xAI;
+  model: OpenAIModel | xAI | OpenAI;
   apiKey: string;
 }
 
