aa90e1a212810fc823875ede645ed94aa1fd084f
Bishnu Suyel <suyel2007@gmail.com>
2025-02-25 14:08:30 +0200

    Wrap the application with AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { AuthProvider } from '../contexts/AuthContext';
+import { Toaster } from 'react-hot-toast';
+import Nav from '../components/Nav';
 
 const inter = Inter({ subsets: ["latin"] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <AuthProvider>
+      <main className={inter.className}>
+        <Nav isDark={false} toggleDarkMode={() => {}} />
+        <Component {...pageProps} />
+      </main>
+      <Toaster position="top-center" />
+    </AuthProvider>
   );
 }
 
