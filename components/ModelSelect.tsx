0fd8d2b9164d006137ee14fa704f4ad0d64d24cc
Kajol sutra dhar <kajol.sd777@gmail.com>
2025-03-03 15:11:34 +0200

    Removed: GPT 4-0125 preview

--- components/ModelSelect.tsx
+++ components/ModelSelect.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const ModelSelect: FC<Props> = ({ model, onChange, isDark }) => {
   const bg = isDark ? 'bg-[#1A1B26]' : 'bg-[#fff]';
   const textColor = isDark ? 'text-neutral-200 ' : 'text-black';
-  
+
   return (
     <select
       className={`${bg} ${textColor} h-[40px] w-fit rounded-md px-4 py-2`}
@@ -22,6 +22,7 @@ export const ModelSelect: FC<Props> = ({ model, onChange, isDark }) => {
       <option value="gpt-4-turbo">GPT-4-Turbo</option>
       <option value="gpt-4o">GPT-4o</option>
       <option value="gpt-4o-mini">GPT-4o Mini</option>
+      {/* <option value="gpt-4-0125-preview">GPT-4-0125-Preview</option> */}
       <option value="o1-preview">GPT-o1-Preview</option>
       <option value="o1-mini">GPT-o1-Mini</option>
       <option value="o3-mini">GPT-o3-mini</option>
