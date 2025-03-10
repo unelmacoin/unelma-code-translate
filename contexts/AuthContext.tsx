fcf4a292611accb73e75d3876e57d8b5354cfee1
Bishnu Suyel <suyel2007@gmail.com>
2025-03-10 12:54:35 +0200

    Add error handling for sign-in & sign-out methods for better error management

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,11 +1,11 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { auth } from '../config/firebase';
 import { User, signOut as firebaseSignOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
- 
+
 export interface AuthContextType {
   user: User | null;
   loading: boolean;
-  // signUpWithEmail: (email: string, password: string) => Promise<void>;
+
   signInWithEmail: (email: string, password: string) => Promise<void>;
   signInWithGoogle: () => Promise<void>;
   signOut: () => Promise<void>;
@@ -14,7 +14,7 @@ export interface AuthContextType {
 const AuthContext = createContext<AuthContextType>({
   user: null,
   loading: true,
-  // signUpWithEmail: async () => {},
+
   signInWithEmail: async () => {},
   signInWithGoogle: async () => {},
   signOut: async () => {},
@@ -23,37 +23,32 @@ const AuthContext = createContext<AuthContextType>({
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
- 
-  // const signUpWithEmail = async (email: string, password: string) => {
-  //   try {
-  //     await createUserWithEmailAndPassword(auth, email, password);
-  //   } catch (error) {
-  //     console.error('Error signing up with email:', error);
-  //   }
-  // };
- 
+
   const signInWithEmail = async (email: string, password: string) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
       console.error('Error signing in with email:', error);
+      throw error;
     }
   };
- 
+
   const signInWithGoogle = async () => {
     try {
       const provider = new GoogleAuthProvider();
       await signInWithPopup(auth, provider);
     } catch (error) {
       console.error('Error signing in with Google:', error);
+      throw error; 
     }
   };
- 
+
   const signOut = async () => {
     try {
       await firebaseSignOut(auth);
     } catch (error) {
       console.error('Error signing out:', error);
+      throw error; 
     }
   };
 
@@ -67,7 +62,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, []);
 
   return (
-    // <AuthContext.Provider value={{ user, loading, signUpWithEmail, signInWithEmail, signInWithGoogle, signOut }}>
     <AuthContext.Provider value={{ user, loading, signInWithEmail, signInWithGoogle, signOut }}>
       {!loading && children}
     </AuthContext.Provider>
