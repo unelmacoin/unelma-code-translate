12a8e714400eb865baf77761e34556f9c7d7ef25
Bishnu Suyel <suyel2007@gmail.com>
2025-04-04 21:54:49 +0300

    Add redirection to login page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,11 +6,13 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      // clientId: process.env.GOOGLE_CLIENT_ID || (() => { throw new Error('GOOGLE_CLIENT_ID is not defined') })(),
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET || (() => { throw new Error('GOOGLE_CLIENT_SECRET is not defined') })(),
     }),
     // Add any other authentication providers like email, GitHub, etc.
   ],
   pages: {
-    signIn: '/auth/signin', // Redirect to a custom sign-in page
+    signIn: '/login', // Redirect to a custom sign-in page
   },
   session: {
     strategy: 'jwt' as SessionStrategy, // Use JWT tokens for session management
