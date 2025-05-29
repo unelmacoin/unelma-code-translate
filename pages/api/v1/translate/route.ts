441fa0d13264addebdfbbbedbc981d272ed13346
Anderson Martin <info@unelmaplatforms.com>
2025-05-29 21:24:31 +0300

    Integrate more translation in API

--- pages/api/v1/translate/route.ts
+++ pages/api/v1/translate/route.ts
@@ -8,14 +8,91 @@ const openai = new OpenAI({
 
 // Supported language pairs
 const SUPPORTED_LANGUAGES = [
+  // Python
   { from: 'python', to: 'javascript' },
   { from: 'javascript', to: 'python' },
   { from: 'python', to: 'java' },
   { from: 'java', to: 'python' },
+  { from: 'python', to: 'typescript' },
+  { from: 'typescript', to: 'python' },
+  { from: 'python', to: 'c++' },
+  { from: 'c++', to: 'python' },
+  { from: 'python', to: 'c#' },
+  { from: 'c#', to: 'python' },
+  { from: 'python', to: 'ruby' },
+  { from: 'ruby', to: 'python' },
+  { from: 'python', to: 'go' },
+  { from: 'go', to: 'python' },
+  
+  // JavaScript/TypeScript
   { from: 'javascript', to: 'java' },
   { from: 'java', to: 'javascript' },
+  { from: 'javascript', to: 'typescript' },
+  { from: 'typescript', to: 'javascript' },
+  { from: 'javascript', to: 'c++' },
+  { from: 'c++', to: 'javascript' },
+  { from: 'javascript', to: 'c#' },
+  { from: 'c#', to: 'javascript' },
+  
+  // Java
+  { from: 'java', to: 'c++' },
+  { from: 'c++', to: 'java' },
+  { from: 'java', to: 'c#' },
+  { from: 'c#', to: 'java' },
+  { from: 'java', to: 'go' },
+  { from: 'go', to: 'java' },
+  { from: 'java', to: 'kotlin' },
+  { from: 'kotlin', to: 'java' },
+  { from: 'java', to: 'scala' },
+  { from: 'scala', to: 'java' },
+  
+  // C/C++
+  { from: 'c++', to: 'c' },
+  { from: 'c', to: 'c++' },
+  { from: 'c++', to: 'c#' },
+  { from: 'c#', to: 'c++' },
+  { from: 'c++', to: 'rust' },
+  { from: 'rust', to: 'c++' },
+  
+  // Web technologies
+  { from: 'html', to: 'javascript' },
+  { from: 'javascript', to: 'html' },
+  { from: 'css', to: 'scss' },
+  { from: 'scss', to: 'css' },
+  { from: 'typescript', to: 'javascript' },
+  { from: 'javascript', to: 'typescript' },
+  
+  // Scripting languages
+  { from: 'ruby', to: 'python' },
+  { from: 'python', to: 'ruby' },
+  { from: 'php', to: 'python' },
+  { from: 'python', to: 'php' },
+  { from: 'perl', to: 'python' },
+  { from: 'python', to: 'perl' },
+  
+  // Systems languages
+  { from: 'rust', to: 'go' },
+  { from: 'go', to: 'rust' },
+  { from: 'rust', to: 'c++' },
+  { from: 'c++', to: 'rust' },
+  { from: 'go', to: 'python' },
+  { from: 'python', to: 'go' },
+  
+  // Legacy languages
   { from: 'cobol', to: 'java' },
   { from: 'java', to: 'cobol' },
+  { from: 'fortran', to: 'python' },
+  { from: 'python', to: 'fortran' },
+  { from: 'pascal', to: 'python' },
+  { from: 'python', to: 'pascal' },
+  
+  // Newer languages
+  { from: 'swift', to: 'kotlin' },
+  { from: 'kotlin', to: 'swift' },
+  { from: 'dart', to: 'javascript' },
+  { from: 'javascript', to: 'dart' },
+  { from: 'typescript', to: 'dart' },
+  { from: 'dart', to: 'typescript' }
 ];
 
 export async function POST(req: Request) {
